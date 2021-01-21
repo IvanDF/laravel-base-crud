b0402fc872307328acc628f1ec8101e5e74db77e
John DoeIvanDF <delfatti.ivan@gmail.com>
2021-01-21 21:34:14 +0100

    Prova

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="hero-body">
         <div class="container">
             <p class="title">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nostrum beatae molestias nisi magni dignissimos tempora impedit repellendus in sed? Facilis libero facere consequatur nulla maxime similique explicabo earum sit?
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate nostrum beatae molestias nisi magni dignissimos tempora impedit repellendus in sed? Facilis libero facere consequatur nulla maxime similique explicabo earum sit?
             </p>
         </div>
     </div>
