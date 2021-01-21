7544c5157106855f949c02f28cad2b809b347069
John DoeIvanDF <delfatti.ivan@gmail.com>
2021-01-21 17:18:42 +0100

    Fix

--- resources/views/partials/main-header.blade.php
+++ resources/views/partials/main-header.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar is-link" role="navigation" aria-label="main navigation">
         <div class="container">
             <div class="navbar-brand">
-                <a class="navbar-item" href="https://bulma.io">
+                <a class="navbar-item" href="{{route('home')}}">
                     <h2 class="title has-text-info-light"> My Blog </h2>
                 </a>
 
